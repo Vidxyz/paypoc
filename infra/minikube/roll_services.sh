88b38e7205c41873a49399476d39da558d68ff63
Vidxyz <vidhyasagar1210@gmail.com>
2026-02-05 20:42:07 -0500

    fix: Add scripts to recreate

---
+++ infra/minikube/roll_services.sh
@@ -0,0 +1,12 @@
+# Rollout restart every single service in parallel
+kubectl rollout restart deployment/payments-service -n payments-platform &
+kubectl rollout restart deployment/ledger-service -n payments-platform &
+kubectl rollout restart deployment/inventory-service -n inventory
+kubectl rollout restart deployment/cart-service -n cart &
+kubectl rollout restart deployment/catalog-service -n catalog &
+kubectl rollout restart deployment/order-service -n order &
+kubectl rollout restart deployment/fulfillment-service -n fulfillment &
+kubectl rollout restart deployment/user-service -n user &
+kubectl rollout restart deployment/admin-console -n ui &
+kubectl rollout restart deployment/frontend -n ui &
+kubectl rollout restart deployment/seller-console -n ui
